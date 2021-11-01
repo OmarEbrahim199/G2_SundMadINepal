dea4ffefd57b0c94daa3cb14b05986aee1ca42fd
Christoffer <s185178@student.dtu.dk>
2021-11-01 12:28:25 +0100

    background update

--- app/src/main/java/com/example/sunmadinepal/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/sunmadinepal/ui/home/HomeFragment.kt
@@ -36,6 +36,10 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+      /*  val textView: TextView = binding.textHome
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })*/
 
 
 
