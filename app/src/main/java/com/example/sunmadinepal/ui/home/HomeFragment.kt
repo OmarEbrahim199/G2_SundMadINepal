b464ab53f7defe28c535050db5f38010fe31d6af
G-morgensol <s205428@student.dtu.dk>
2021-11-01 12:25:04 +0100

    Removed hometext from  homefragment

--- app/src/main/java/com/example/sunmadinepal/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/sunmadinepal/ui/home/HomeFragment.kt
@@ -36,10 +36,6 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
 
 
 
