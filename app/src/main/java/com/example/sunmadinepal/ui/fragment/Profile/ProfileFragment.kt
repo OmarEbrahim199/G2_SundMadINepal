8bc380c181f1a78ed27a9b6be3ec1f8a2cc614ce
s205476 <s205476@student.dtu.dk>
2022-01-04 18:05:17 +0100

    Tilføjet kalender til add doctors appointment

--- app/src/main/java/com/example/sunmadinepal/ui/fragment/Profile/ProfileFragment.kt
+++ app/src/main/java/com/example/sunmadinepal/ui/fragment/Profile/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.example.sunmadinepal.ui.fragment.Profile
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -13,6 +14,7 @@ import androidx.lifecycle.Observer
 
 import com.example.sunmadinepal.databinding.FragmentProfileBinding
 import com.example.sunmadinepal.ui.ViewModel.ProfileViewModel
+import java.util.*
 
 class ProfileFragment : Fragment() {
 
@@ -29,6 +31,19 @@ class ProfileFragment : Fragment() {
         profileViewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        val date = Calendar.getInstance()
+        val year = date.get(Calendar.YEAR)
+        val month = date.get(Calendar.MONTH)
+        val day = date.get(Calendar.DAY_OF_MONTH)
+
+        binding.addAppointment.setOnClickListener { val datePickerDialog = DatePickerDialog(requireContext(), { view, savedYear, savedMonth, savedDay ->
+            binding.date1.setText(""+ savedDay +"-"+ savedMonth +"-"+ savedYear)
+        }, year, month, day)
+
+            datePickerDialog.show()
+
+        }
 
 
 /*
