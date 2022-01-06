87e7fa3b36a6be4d810dc79d7d6f7c78e96bd7d9
G-morgensol <s205428@student.dtu.dk>
2022-01-06 12:15:12 +0100

    Made DoctorAppointment data class

--- app/src/main/java/com/example/sunmadinepal/framework/db/DoctorAppointment.kt
+++ app/src/main/java/com/example/sunmadinepal/framework/db/DoctorAppointment.kt
@@ -1,8 +1,16 @@
 package com.example.sunmadinepal.framework.db
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 
+@Entity(tableName = "doctorAppointments")
 data class DoctorAppointment(
+    @PrimaryKey val dId: Int,
 
-
+    val year: Int?,
+    val month: Int?,
+    val day: Int?,
+    val hour: Int?,
+    val minute: Int?
 )
