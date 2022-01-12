c7872d0683d81c33332a2f827a639485a1046f5a
s205476 <s205476@student.dtu.dk>
2022-01-12 10:17:12 +0100

    Fix merge

--- app/src/main/java/com/example/sunmadinepal/framework/db/DoctorAppointmentDao.kt
+++ app/src/main/java/com/example/sunmadinepal/framework/db/DoctorAppointmentDao.kt
@@ -6,7 +6,7 @@ import java.util.*
 import java.util.concurrent.Flow
 
 
-
+//MERGE FIIIX
 // Based on https://developer.android.com/training/data-storage/room/accessing-data#kotlin
 @Dao
 interface DoctorAppointmentDao {
